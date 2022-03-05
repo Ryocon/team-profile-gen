d7f71cb71b18ea6eaae5b937aff1ff625f8f30d2
roconn <roconn25@gmail.com>
2022-03-05 20:04:53 +0000

    updated json with 'jest' script + employee test

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,10 +1,24 @@
 const Employee = require('../lib/Employee')
 
-describe('Employee has the correct data applied', () => {
-    const testEmployee = new Employee('TestyMcTestFace', 1, 'test@email.com')
+const employee = new Employee('TestyMcTestFace', 1, 'test@email.com')
 
-    expect(this.name).toEqual('TestyMcTestFace')
-    expect(this.id).toEqual(number)
-    expect(this.email).toEqual('test@email.com')
-}
+describe('Employee name', () => {
+    it('returns the name', () => {
+    expect(employee.name).toEqual('TestyMcTestFace')
+        }   )
+    }
+)
+
+describe('Employee ID', () => {
+    it('returns the ID of the employee', () => {
+        expect(employee.id).toEqual(expect.any(Number))
+        }   )
+    }
+)
+
+describe('Employee email', () => {
+    it('returns the email of the employee', () => {
+        expect(employee.email).toEqual('test@email.com')
+        }   )
+    }
 )
